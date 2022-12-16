f351a57fe5f8927287da08a24eff9a2a576e2b1e
4FRO <drotich@ucsc.edu>
2022-12-16 15:32:27 -0800

    Setup Axios, connected to fake server, getBlogPosts useEffect

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://345a-2601-644-300-88e0-34d6-3993-69b4-ab50.ngrok.io',
+});
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,17 +1,15 @@
-import React, { useContext } from 'react';
-import {
-  View,
-  StyleSheet,
-  FlatList,
-  TouchableOpacity,
-} from 'react-native';
+import React, { useContext, useEffect } from 'react';
+import { View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Ionicons } from '@expo/vector-icons';
 
 import BlogListItem from '../components/BlogListItem';
 
 const IndexScreen = ({ navigation }) => {
-  const { state, deleteBlogPost } = useContext(Context);
+  const { state, deleteBlogPost, getBlogPosts } = useContext(Context);
+  useEffect(() => {
+    getBlogPosts();
+  }, []);
 
   return (
     <View>
